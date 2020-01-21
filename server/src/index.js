658e7fa7ec6c370daa6bf37806b4b1d11ceb4274
BigUncleYemi <olayemioyebanji911@gmail.com>
2020-01-21 23:57:10 +0100

    lint check

--- server/src/index.js
+++ server/src/index.js
@@ -1,33 +1,30 @@
-// Require the framework and instantiate it
 const routes = require("./routes");
-const path = require("path");
-const fastify = require('fastify')({
+const fastify = require("fastify")({
   logger: true
-})
+});
 
 routes.forEach((route, index) => {
- fastify.route(route)
-})
+  fastify.route(route);
+});
 
-// Declare a route
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world todo api' }
-})
+const swagger = require("./config/swagger");
+fastify.register(require("fastify-swagger"), swagger.options);
 
-const mongoose = require('mongoose')
-// Connect to DB
-mongoose.connect("mongodb://localhost/tododlist")
- .then(() => console.log("MongoDB connected…"))
-   .catch(err => console.log(err))
- 
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/tododlist")
+  .then(() => console.log("MongoDB connected…"))
+  .catch(err => console.log(err));
+
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3001)
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    await fastify.listen(3000);
+    fastify.swagger();
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-}
-start()
+};
+start();
--- server/src/models/todo.js
+++ server/src/models/todo.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
-  title: String,
-  desc: String,
-  Date: Date,
-  time: String,
-  edited: { type: Boolean, default: false},
-})
+  title: {type: String, required: true},
+  desc: {type: String, required: false},
+  Date: {type: Date, required: true},
+  time: {type: String, required: true},
+  edited: { type: Boolean, default: false }
+});
 
-module.exports = mongoose.model('TodoList', todoSchema)
+module.exports = mongoose.model("TodoList", todoSchema);
